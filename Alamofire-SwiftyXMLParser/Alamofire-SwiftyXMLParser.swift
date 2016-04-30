0636501e36e704ab0e6abefa1a642d993a8ff7f5
Kazuhiro Hayashi <k.hayashi.info@gmail.com>
2016-05-01 04:55:20 +0900

    add file

--- Alamofire-SwiftyXMLParser/Alamofire-SwiftyXMLParser.swift
+++ Alamofire-SwiftyXMLParser/Alamofire-SwiftyXMLParser.swift
@@ -7,3 +7,66 @@
 //
 
 import Foundation
+import Alamofire
+import SwiftyXMLParser
+
+extension Request {
+    
+    /**
+     Creates a response serializer that returns a XML object constructed from the response data using
+     `SwiftyXMLParser` with the specified reading options.
+     
+     - parameter option: The XML serialization trimming character options. nothing by default.
+     
+     - returns: A XML.Accessor object response serializer.
+     */
+    public static func XMLResponseSerializer(
+        option option: NSCharacterSet? = nil)
+        -> ResponseSerializer<XML.Accessor, XML.Error>
+    {
+        return ResponseSerializer { _, response, data, error in
+            guard error == nil else {
+                return .Failure(.ParseError)
+            }
+            
+            if let response = response where response.statusCode == 204 {
+                return .Success(XML.parse(NSData()))
+            }
+            
+            guard let validData = data where validData.length > 0 else {
+                return .Failure(.ParseError)
+            }
+            
+            let xml: XML.Accessor
+            if let option = option {
+                xml = XML.parse(validData, trimming: option)
+            } else {
+                xml = XML.parse(validData)
+            }
+            return .Success(xml)
+        }
+    }
+    
+    /**
+     Adds a handler to be called once the request has finished.
+     
+     - parameter options:           The XML serialization trimming character option. nothing by default.
+     - parameter completionHandler: A closure to be executed once the request has finished.
+     
+     - returns: The request.
+     */
+    public func responseXML(
+        queue queue: dispatch_queue_t? = nil,
+              option: NSCharacterSet? = nil,
+              completionHandler: Response<XML.Accessor, XML.Error> -> Void)
+        -> Self
+    {
+        return response(
+            queue: queue,
+            responseSerializer: Request.XMLResponseSerializer(option: option),
+            completionHandler: completionHandler
+        )
+    }
+}
+
+
