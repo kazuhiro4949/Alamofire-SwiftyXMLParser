4bae9b15b3a9f70e71ca16f16af824ffd7361dbf
Kazuhiro Hayashi <k.hayashi.info@gmail.com>
2016-05-01 04:02:40 +0900

    add submodule

---
+++ Alamofire-SwiftyXMLParser/Alamofire-SwiftyXMLParser.h
@@ -0,0 +1,19 @@
+//
+//  Alamofire-SwiftyXMLParser.h
+//  Alamofire-SwiftyXMLParser
+//
+//  Created by Kazuhiro Hayashi on 4/30/16.
+//  Copyright © 2016 Kazuhiro Hayashi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Alamofire-SwiftyXMLParser.
+FOUNDATION_EXPORT double Alamofire_SwiftyXMLParserVersionNumber;
+
+//! Project version string for Alamofire-SwiftyXMLParser.
+FOUNDATION_EXPORT const unsigned char Alamofire_SwiftyXMLParserVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Alamofire_SwiftyXMLParser/PublicHeader.h>
+
+
---
+++ Alamofire-SwiftyXMLParser/Alamofire-SwiftyXMLParser.swift
@@ -0,0 +1,9 @@
+//
+//  Alamofire-SwiftyXMLParser.swift
+//  Alamofire-SwiftyXMLParser
+//
+//  Created by Kazuhiro Hayashi on 5/1/16.
+//  Copyright © 2016 Kazuhiro Hayashi. All rights reserved.
+//
+
+import Foundation
